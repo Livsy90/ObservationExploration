81102f77f9c7217704ea8418f93bf925dae5d8bc
Livsy90 <artmir90@gmail.com>
2025-08-27 13:26:20 +0400

    Initial Commit

---
+++ ObservationExploration/ObservationExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationExplorationApp.swift
+//  ObservationExploration
+//
+//  Created by Artem Mir on 27.08.25.
+//
+
+import SwiftUI
+
+@main
+struct ObservationExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
